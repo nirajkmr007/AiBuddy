a89387445ba5843ec732aef32ad04008df4920e3
Niraj Kumar <niraj.kumar@BRILLIO.COM>
2018-08-04 09:37:35 +0530

    First commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=aibuddy-Web-Deploy.pubxml -p:Password=z2Pshb9q2xGQcluCeN85RGpy7aBMLtxWiNWZqPvfhl8S0rgP086xFSLSQu3R
+
