22f1589742b9b5dd694c1b4ab570914c824854b0
niraj kumar <nirajkmr007@gmail.com>
2018-08-04 17:10:46 +0530

    VS test

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                await context.PostAsync($"{this.count++}: what {message.Text} means?");
                 context.Wait(MessageReceivedAsync);
             }
         }
