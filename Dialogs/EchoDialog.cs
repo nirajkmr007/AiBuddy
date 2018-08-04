49a3132e308c6ae85036d63e9ebc2a99acc73bed
Niraj Kumar <niraj.kumar@BRILLIO.COM>
2018-08-04 11:53:05 +0530

    text change

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: You said {message.Text} \n i donot understand that even though \n i love you Neeru");
+                await context.PostAsync($"{this.count++}: You said {message.Text}");
                 context.Wait(MessageReceivedAsync);
             }
         }
